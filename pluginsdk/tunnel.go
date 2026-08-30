a0fc02269f3406333c207e8cb8927510591b04ab
erniu <57487816+erniuu@users.noreply.github.com>
2026-08-30 19:01:23 +0800

    Update tunnel.go

--- pluginsdk/tunnel.go
+++ pluginsdk/tunnel.go
@@ -5,10 +5,11 @@ import "io"
 type TunnelTS interface {
 	TunnelChannelExists(channelId int) bool
 	TunnelStart(tunnelId string) (string, error)
+	
 	TunnelCreateSocks4(agentId string, info string, lhost string, lport int) (string, error)
 	TunnelCreateSocks5(agentId string, info string, lhost string, lport int, useAuth bool, username string, password string) (string, error)
-
 	TunnelCreateLportfwd(agentId string, info string, lhost string, lport int, thost string, tport int) (string, error)
+	TunnelCreateHttp(agentId string, info string, lhost string, lport int, useAuth bool, username string, password string) (string, error)
 
 	TunnelStopSocks(agentId string, port int)
 	TunnelStopLportfwd(agentId string, port int)
